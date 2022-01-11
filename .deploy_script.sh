8a4e332e99e08b87695171e1e6bb7c2ab309fe88
Kaushik Khobragade <awskaushik@gmail.com>
2022-01-11 16:03:17 +0000

    Update .deploy_script.sh

--- .deploy_script.sh
+++ .deploy_script.sh
@@ -1,1 +1,6 @@
-ssh git-user@$SERVER "cd /home/ubuntu && sudo touch kaushik && sudo touch newfile"
+ssh git-user@$SERVER "sudo apt-get install awscli -y && sudo aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && sudo aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY && sudo aws s3 cp s3://mygitlabrunner/reactjs-frontend.zip ./home/ubuntu && unzip reactjs-frontend.zip && sudo service nginx restart"
+
+
+
+
+ #ssh ubuntu@$SERVER "aws s3 cp s3://$BUCKET_NAME/myfirstproject.zip .  && unzip myfirstproject.zip && cd myfirstproject && npm install express --save && forever stop index.js || true && forever start index.js "
