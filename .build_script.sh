78e7dcca1a682864e37d6dda23543c9b12e8b702
Kaushik Khobragade <awskaushik@gmail.com>
2022-01-11 16:25:57 +0000

    Update .build_script.sh

--- .build_script.sh
+++ .build_script.sh
@@ -2,15 +2,16 @@
 #update your token and git url in the clone command below
 #update your s3 bucket name
 
-git clone -b develop  https://gitlab+deploy-token-497024:glpat-LzXgf9mE8XZgYBExnzJw@gitlab.com/awskaushik/mack-node.git
-cd mack-node
+git clone -b develop  https://gitlab+deploy-token-497024:glpat-7Md6Lz2rSLMzHqUdxiye@gitlab.com/awskaushik/mack-react.git
+cd reactjs-frontend
 npm install express --save
 npm config set fund false --global
+sudo npm run build
 cd ../
-apt-get update -y
-apt-get install zip -y
-apt-get install awscli -y
-aws configure set aws_access_key_id AKIAXMJWKFPCXXGJ3ZMV
-aws configure set aws_secret_access_key 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
-zip -r mack-node.zip mack-node/ -x '*/node_modules/*' -x '*/.git/*'
-aws s3 cp mack-node.zip s3://gitrunnerbucket/
+sudo apt-get update -y
+sudo apt-get install zip -y
+sudo apt-get install awscli -y
+sudo aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+sudo aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+sudo zip -r reactjs-frontend.zip reactjs-frontend/ -x '*/node_modules/*' -x '*/.git/*'
+sudo aws s3 cp reactjs-frontend.zip s3://mygitlabrunner/
