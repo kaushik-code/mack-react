216fc2e0108d518ef22f150469f9cc5189e08072
Kaushik Khobragade <awskaushik@gmail.com>
2022-01-13 08:02:33 +0000

    Update .build_script.sh

--- .build_script.sh
+++ .build_script.sh
@@ -4,17 +4,16 @@
 
 git clone -b develop  https://gitlab+deploy-token-497024:glpat-7Md6Lz2rSLMzHqUdxiye@gitlab.com/awsabhilash3/reactjs-frontend.git
 cd reactjs-frontend
-git checkout branch develop
-npm ci
+npm install
 #npm install express --save
 #npm config set fund false --global
 npm run build
 cd ../
-apt-get update -y
-apt-get install zip -y
-apt-get install awscli -y
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+#apt-get update -y
+#apt-get install zip -y
+#apt-get install awscli -y
+#aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+#aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 zip -r reactjs-frontend.zip reactjs-frontend/ -x '*/node_modules/*' -x '*/.git/*'
 aws s3 cp reactjs-frontend.zip s3://mygitlabrunner/
 
